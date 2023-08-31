16a4513c6b77810f1815c9f415fc6eb7f0154b1b
TanyaYurkevich <141036160+TanyaYurkevich@users.noreply.github.com>
2023-08-31 11:19:15 +0300

    Update Runner.java

--- scr/com/teachmeskills/Lesson10_HW/Runner.java
+++ scr/com/teachmeskills/Lesson10_HW/Runner.java
@@ -5,14 +5,14 @@ public class Runner {
         Util_Doc doc = new Util_Doc("5556-kiTh-2598-abCm-8u9p");
         Util_Doc doc1 = new Util_Doc("1246-kiNh-2498-tiEm-8r7p");
 
-        String s = Util_Doc.outputFirstBlock(doc.DocNumber);
-        String s1 = Util_Doc.outputFirstBlock(doc1.DocNumber);
+        String s = Util_Doc.outputFirstBlock(doc.getDocNumber());
+        String s1 = Util_Doc.outputFirstBlock(doc1.getDocNumber());
         System.out.println(s + s1);
-        String s2 = Util_Doc.replaceLetters(doc1.DocNumber);
+        String s2 = Util_Doc.replaceLetters(doc1.getDocNumber());
         System.out.println(s2);
-        Util_Doc.outputLetters1(doc1.DocNumber);
-        Util_Doc.outputLetters2(doc.DocNumber);
-        Util_Doc.checkCharactersDoc(doc1.DocNumber);
+        Util_Doc.outputLetters1(doc1.getDocNumber());
+        Util_Doc.outputLetters2(doc.getDocNumber());
+        Util_Doc.checkCharactersDoc(doc1.getDocNumber());
         Util_Doc.checkStartEnd(doc.DocNumber);
         String s3 = Util_Doc.printMaxLongWorld("I like to study!");
         System.out.println(s3);
